504b95601905826727213625b9ca80bfd0ed849c
“cissamil“ <clarissamorita1@gmail.com>
2026-06-11 18:08:51 -0300

    fix: corrige minor-meridian que estava como submodulo quebrado

---
+++ minor-meridian/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    
+    site: 'https://cissamil.github.io',
+    base: '/ds881-curriculo-GRR20245515',
+});
